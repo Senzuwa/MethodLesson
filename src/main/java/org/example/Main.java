73af7e4620f48cc09eff5e5284aafcd7e38fa24e
Senzuwa <azkuth@icloud.com>
2023-11-30 15:13:38 +0900

    name->MleagerList/suzuki.Y,setokumaを追加

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,20 +4,19 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        List<String> names = List.of("nakabayashi","suzuki", "honda","mizuhara","kobayashi");
+        List<String> MleagerList = List.of("nakabayashi","suzuki.T", "honda","mizuhara","kobayashi","suzuki.Y","setokuma");
 
-        names.stream().map(String::toUpperCase).forEach(System.out::println);
-        names.stream().map(String::toLowerCase).forEach(System.out::println);
+        MleagerList.stream().map(String::toUpperCase).forEach(System.out::println);
+        MleagerList.stream().map(String::toLowerCase).forEach(System.out::println);
 
-        List<String> SortedResult = names.stream().sorted().toList();
+        List<String> SortedResult = MleagerList.stream().sorted().toList();
         SortedResult.forEach(System.out::println);
 
-        long count = names.stream().filter(List.of("honda","mizuhara")::contains).count();
+        long count = MleagerList.stream().filter(List.of("honda","mizuhara")::contains).count();
         System.out.println(count);
 
-        boolean hasbayashi = names.stream().anyMatch(List.of("kobayashi")::contains);
+        boolean hasbayashi = MleagerList.stream().anyMatch(List.of("kobayashi")::contains);
         System.out.println(hasbayashi);
-
 
 
     }
